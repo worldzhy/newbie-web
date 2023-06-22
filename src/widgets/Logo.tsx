bcf332ba6f450144a5200ae3ff84daab0055381f
ijborda <imari.borda2018@gmail.com>
2023-06-22 23:43:23 +0800

    Make logo clickable

--- src/widgets/Logo.tsx
+++ src/widgets/Logo.tsx
@@ -1,9 +1,10 @@
 import React, { type ReactElement } from 'react';
+import Link from 'next/link';
 import { Typography } from '@mui/material';
 import styles from './Logo.module.css';
 
 const Logo = (): ReactElement => {
-  return <Typography className={styles.logo} variant='h1'>Logo</Typography>;
+  return <Link href='/' className={styles.logo}><Typography variant='h1' className={styles.text}>Logo</Typography></Link>;
 };
 
 export default Logo;
