c8c2fcfb87efebb184da12d5ee70821bd2b40cfb
ijborda <imari.borda2018@gmail.com>
2023-06-23 19:58:37 +0800

    Add page tabs

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,19 +1,68 @@
-import React, { type ReactElement } from 'react';
-import { Typography } from '@mui/material';
+import React, { type ReactElement, type SyntheticEvent } from 'react';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { Box, Tab } from '@mui/material';
 
 import LayoutDashboard from '@/widgets/LayoutDashboard';
 import styles from '@/styles/Team.module.css';
+import styleConfig from '@/constants/styleConfig';
 
-const Page = (): React.ReactElement => {
+const Page = (): ReactElement => {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: SyntheticEvent, newValue: string): void => {
+    setValue(newValue);
+  };
+
   return (
-    <>
-      <Typography paragraph>
-        Content
-      </Typography>
-      <Typography paragraph>
-        Content
-      </Typography>
-    </>
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box>
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            sx={{
+              '& .MuiTabs-flexContainer': { gap: 2 },
+              '& .MuiTabs-indicator': { display: 'none' }
+            }}
+            variant='scrollable'
+            scrollButtons="auto"
+          >
+              {['Members', 'Roles', 'About us'].map((label: string, index: number) => (
+                <Tab
+                  key={label}
+                  className={styles.tabItems}
+                  label={label}
+                  value={(index + 1).toString()}
+                  sx={{
+                    '&.Mui-selected': {
+                      color: styleConfig.color.primaryWhiteColor,
+                      background: styleConfig.color.primaryBlackColor
+                    }
+                  }}
+                />
+              ))}
+          </TabList>
+        </Box>
+        <TabPanel
+          value="1"
+          sx={{ border: 2, borderColor: styleConfig.color.primaryGrayColor, marginTop: 2 }}
+        >
+          Item One
+        </TabPanel>
+        <TabPanel
+          value="2"
+          sx={{ border: 2, borderColor: styleConfig.color.primaryGrayColor, marginTop: 2 }}
+        >
+          Item Two
+        </TabPanel>
+        <TabPanel
+          value="3"
+          sx={{ border: 2, borderColor: styleConfig.color.primaryGrayColor, marginTop: 2 }}
+        >
+          Item Three
+        </TabPanel>
+      </TabContext>
+    </Box>
   );
 };
 
