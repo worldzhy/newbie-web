a0e7a34db60623291fd69bbd90ab1789d0335959
ijborda <imari.borda2018@gmail.com>
2023-06-24 10:42:17 +0800

    Implement role

--- src/widgets/Team/Role.tsx
+++ src/widgets/Team/Role.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, type ReactElement } from 'react';
-import { Stack } from '@mui/material';
+import { Stack, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import ButtonCustom from '@/components/ButtonCustom/ButtonCustom';
+import InputTextCustom from '@/components/InputTextCustom/InputTextCustom';
+import LoadingButtonCustom from '@/components/LoadingButtonCustom/LoadingButtonCustom';
 import TableCustom from '@/components/TableCustom/TableCustom';
+import { sendRequest, showError } from '@/shared/libs/mixins';
 import Role from '@/shared/libs/role';
 
 const TeamRoles = (): ReactElement => {
@@ -13,10 +16,33 @@ const TeamRoles = (): ReactElement => {
   /**
   * States
   */
+  const [isProcessing, setIsProcessing] = React.useState(false);
+  const [modal, setModal] = React.useState(false);
+  const [newFetch, setNewFetch] = React.useState(false);
   const [rows, setRows] = useState([] as Array<Record<string, any>>);
+  const [roleName, setRoleName] = React.useState('');
 
   /**
   * Handlers
+  */
+  const modalOpenHandler = (): void => {
+    setModal(true);
+  };
+
+  const modalCloseHandler = (): void => {
+    setModal(false);
+  };
+
+  const createRole = async (): Promise<void> => {
+    await sendRequest(setIsProcessing, async () => {
+      await (new Role()).create(roleName);
+    });
+    modalCloseHandler();
+    setNewFetch(!newFetch);
+  };
+
+  /**
+  * Data Fetching
   */
   useEffect(() => {
     const startFetching = async (): Promise<void> => {
@@ -30,8 +56,8 @@ const TeamRoles = (): ReactElement => {
           const fetchedRows = roles.map(role => createData(role.name, role.description ?? 'xxx', 'Edit'));
           setRows(fetchedRows);
         }
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      } catch (err: unknown) {
+        showError(err);
       }
     };
     let ignore = false;
@@ -40,25 +66,57 @@ const TeamRoles = (): ReactElement => {
     return () => {
       ignore = true;
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [newFetch]);
 
   return (
-    <Stack
-      direction='column'
-      spacing={2}
-      alignItems="flex-end"
-    >
-      <ButtonCustom
-        color='dark'
-        // onClick={() => {
-        //   void addRoleHandler();
-        // }}
+    <>
+      <Stack
+        direction='column'
+        spacing={2}
+        alignItems="flex-end"
       >
-        New role
-      </ButtonCustom>
-      <TableCustom headers={headers} rows={rows} ></TableCustom>
-    </Stack>
+        <ButtonCustom
+          color='dark'
+          onClick={() => {
+            modalOpenHandler();
+          }}
+        >
+          New role
+        </ButtonCustom>
+        <TableCustom headers={headers} rows={rows} ></TableCustom>
+      </Stack>
+      <Dialog open={modal} onClose={modalCloseHandler}>
+        <DialogTitle>Role Name</DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            sx={{ paddingBottom: '12px' }}
+          >
+            Enter the desired role name in the designated field to create a role.
+          </DialogContentText>
+          <InputTextCustom
+            label='Role name'
+            variant='outlined'
+            value={roleName}
+            type='text'
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setRoleName(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <DialogActions
+          sx={{ padding: '16px 24px' }}
+        >
+          <LoadingButtonCustom
+            loading={isProcessing}
+            onClick={() => {
+              void createRole();
+            }}>
+              Confirm
+          </LoadingButtonCustom>
+          <ButtonCustom color='dark' onClick={modalCloseHandler}>Cancel</ButtonCustom>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
